b423a3292520385cd152184379e53c65595930c3
kevinbasoftcat <kevinba@softcat.com>
2022-07-27 13:20:04 +0100

    Updates for basic testing

--- main.tf
+++ main.tf
@@ -69,7 +69,6 @@ resource "azurerm_site_recovery_protection_container_mapping" "container-mapping
   recovery_source_protection_container_name = azurerm_site_recovery_protection_container.primary.name
   recovery_target_protection_container_id   = azurerm_site_recovery_protection_container.secondary.id
   recovery_replication_policy_id            = azurerm_site_recovery_replication_policy.policy.id
-  tags                                      = var.tags
 }
 
 resource "azurerm_site_recovery_network_mapping" "network-mapping" {
