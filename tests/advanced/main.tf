c1a33ef60775805318f583df4f1f9597f6d84137
kevinbasoftcat <kevinba@softcat.com>
2022-08-11 08:41:46 +0100

    Switched to main branch for deployment tests

--- tests/advanced/main.tf
+++ tests/advanced/main.tf
@@ -89,7 +89,7 @@ module "vm" {
 }
 
 module "asr" {
-    source                          = "github.com/SoftcatMS/terraform-azure-site-recovery?ref=data-disk-multiple-vms"
+    source                          = "github.com/SoftcatMS/terraform-azure-site-recovery"
     location_primary                = "uksouth"
     location_secondary              = "westeurope"
     asr_cache_resource_group_name   = azurerm_resource_group.rg-vm-test-advanced.name
