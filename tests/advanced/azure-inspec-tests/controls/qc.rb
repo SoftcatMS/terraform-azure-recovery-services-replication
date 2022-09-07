e0ff1621b5e7504894622ec5da1ce2c8e6ca4797
kevinbasoftcat <kevinba@softcat.com>
2022-09-07 14:45:00 +0100

    Adjusted Inspec to fix Advanced QC error

--- tests/advanced/azure-inspec-tests/controls/qc.rb
+++ tests/advanced/azure-inspec-tests/controls/qc.rb
@@ -2,11 +2,13 @@
 
 # Test values
 
+resource_group1 = 'rg-test-asr-advanced-resources'
+
 describe azure_resource_group(name: 'rg-ukw-asr-test-advanced') do
   it { should exist }
 end
 
-describe azure_virtual_machine(resource_group: resource_group2, name: 'wintest-vm-adv') do
+describe azure_virtual_machine(resource_group: resource_group1, name: 'wintest-vm-adv') do
   it { should exist }
   its('os_disk_name') { should match 'wintest-vm-adv-osdisk' }
 
